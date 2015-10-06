4f4d57a0050b4a34f2757b20a744f65ca9eefac2
yadongshao <348829719@qq.com>
2015-10-06 23:43:58 +0800

    Create 链表遍历.cpp

---
+++ 链表遍历.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <stack>
+#include <algorithm>
+using namespace std;
+struct node{
+	int data;
+	node *next;
+	node(int data){
+		this->data=data;
+		next=NULL;
+	}
+};
+node *init(int num){
+	int  i=1;
+	node *head=new node(0);
+	while (i<=num)
+	{
+		head->next=new node(i++);
+		head=head->next;
+	}
+	return head;
+}
